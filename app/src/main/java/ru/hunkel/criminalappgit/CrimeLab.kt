a6271aca76ae970709faaee0321b3ee58e4f9fe9
hunkel <kolan19988@yandex.ru>
2019-04-18 13:46:20 +0300

    added crime lab that store crimes

---
+++ app/src/main/java/ru/hunkel/criminalappgit/CrimeLab.kt
@@ -0,0 +1,44 @@
+package ru.hunkel.criminalappgit
+
+import android.content.Context
+import java.util.*
+
+class CrimeLab private constructor(context: Context){
+    companion object{
+        private var sCrimeLab: CrimeLab? = null
+
+        fun get(context: Context): CrimeLab? {
+            if(sCrimeLab == null){
+                sCrimeLab = CrimeLab(context)
+
+            }
+            return sCrimeLab
+        }
+    }
+    init {
+        testData()
+    }
+    private fun testData(){
+        for(i in 1..101){
+            var c = Crime()
+            c.mTitle = "Crime #$i"
+            c.mSolved = (i%2==0)
+            mCrimes.add(c)
+        }
+    }
+    private var mCrimes: MutableList<Crime> = mutableListOf()
+
+    fun getCrimes() : List<Crime>{
+        return mCrimes
+    }
+    fun getCrimeById(id: UUID):Crime{
+        var crime = Crime()
+        for (c in mCrimes){
+            if(id == c.mId){
+                crime = c
+                break
+            }
+        }
+        return crime
+    }
+}
