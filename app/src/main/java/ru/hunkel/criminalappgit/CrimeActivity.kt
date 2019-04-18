e6d86c326e4e2478dadf588939bf29eacf2d1646
hunkel <kolan19988@yandex.ru>
2019-04-18 12:38:49 +0300

    add code that run fragment crime

--- app/src/main/java/ru/hunkel/criminalappgit/CrimeActivity.kt
+++ app/src/main/java/ru/hunkel/criminalappgit/CrimeActivity.kt
@@ -2,11 +2,23 @@ package ru.hunkel.criminalappgit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
 
 class CrimeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fm = supportFragmentManager
+        var fragment = fm.findFragmentById(R.id.fragment_container)
+
+        if(fragment==null){
+            fragment = CrimeFragment()
+            fm.beginTransaction()
+                .add(R.id.fragment_container,fragment)
+                .commit()
+        }
+
     }
 }
