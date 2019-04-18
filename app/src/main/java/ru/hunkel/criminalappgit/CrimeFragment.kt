f75a8c567ca6861c2da8be21d1280e9ef488c5d1
hunkel <kolan19988@yandex.ru>
2019-04-18 12:38:02 +0300

    add crime fragment manager class

---
+++ app/src/main/java/ru/hunkel/criminalappgit/CrimeFragment.kt
@@ -0,0 +1,58 @@
+package ru.hunkel.criminalappgit
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+
+class CrimeFragment : Fragment() {
+    //TODO(ERROR)
+    private var mCrime: Crime = Crime()
+    //TODO(ERROR)
+    private var mTitleField: EditText? = null
+    //TODO(ERROR)
+    private var mDateButton: Button? = null
+    //TODO(ERROR)
+    private var mSolvedCheckBox: CheckBox? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_crime,container,false)
+        //init
+        mTitleField = v.findViewById(R.id.crime_title)
+        mDateButton = v.findViewById(R.id.crime_date)
+        mSolvedCheckBox = v.findViewById(R.id.crime_solved)
+
+        //set up
+        mDateButton?.text = mCrime.mDate
+        mDateButton?.isEnabled = false
+
+        //listeners
+        mSolvedCheckBox?.setOnCheckedChangeListener { _, isChecked ->
+            mCrime.mSolved = isChecked
+        }
+        mTitleField?.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                mTitleField?.setText(s)
+            }
+        })
+        return v
+    }
+}
