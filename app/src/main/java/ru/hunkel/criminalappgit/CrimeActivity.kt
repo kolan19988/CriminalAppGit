7f74deac6541f0dad69b115c191cae2f9b7c991c
hunkel <kolan19988@yandex.ru>
2019-04-19 13:57:00 +0300

    added create fragment by id

--- app/src/main/java/ru/hunkel/criminalappgit/CrimeActivity.kt
+++ app/src/main/java/ru/hunkel/criminalappgit/CrimeActivity.kt
@@ -1,9 +1,23 @@
 package ru.hunkel.criminalappgit
 
+import android.content.Context
+import android.content.Intent
 import android.support.v4.app.Fragment
+import java.util.*
 
 class CrimeActivity : SingleFragmentActivity() {
+    companion object{
+        private const val EXTRA_CRIME_ID = "crime_id"
+
+        fun newIntent(packageContext: Context, crimeId: UUID):Intent{
+            val intent = Intent(packageContext,CrimeActivity::class.java)
+            intent.putExtra(EXTRA_CRIME_ID,crimeId)
+            return intent
+        }
+    }
+
     override fun createFragment(): Fragment {
-        return CrimeFragment()
+        val crimeId = intent.getSerializableExtra(EXTRA_CRIME_ID) as UUID
+        return CrimeFragment.newInstance(crimeId)
     }
 }
